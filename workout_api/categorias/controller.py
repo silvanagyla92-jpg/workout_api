dbd01df8d1d6967ef4af961def2603dd985b1384
Nágyla Silva <silvanagyla92@gmail.com>
2026-08-10 21:33:05 -0300

    Improve error handling in categoria endpoints
    Refactor post, query, and get functions for better error handling and code structure.

--- workout_api/categorias/controller.py
+++ workout_api/categorias/controller.py
@@ -1,60 +1,101 @@
 from uuid import uuid4
+
 from fastapi import APIRouter, Body, HTTPException, status
 from pydantic import UUID4
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.future import select
+
 from workout_api.categorias.schemas import CategoriaIn, CategoriaOut
 from workout_api.categorias.models import CategoriaModel
-
 from workout_api.contrib.dependencies import DatabaseDependency
-from sqlalchemy.future import select
+
 
 router = APIRouter()
 
+
 @router.post(
-    '/', 
+    '/',
     summary='Criar uma nova Categoria',
     status_code=status.HTTP_201_CREATED,
     response_model=CategoriaOut,
 )
 async def post(
-    db_session: DatabaseDependency, 
+    db_session: DatabaseDependency,
     categoria_in: CategoriaIn = Body(...)
 ) -> CategoriaOut:
-    categoria_out = CategoriaOut(id=uuid4(), **categoria_in.model_dump())
-    categoria_model = CategoriaModel(**categoria_out.model_dump())
-    
-    db_session.add(categoria_model)
-    await db_session.commit()
+
+    categoria_out = CategoriaOut(
+        id=uuid4(),
+        **categoria_in.model_dump()
+    )
+
+    categoria_model = CategoriaModel(
+        **categoria_out.model_dump()
+    )
+
+    try:
+        db_session.add(categoria_model)
+        await db_session.commit()
+
+    except IntegrityError:
+        await db_session.rollback()
+
+        raise HTTPException(
+            status_code=status.HTTP_303_SEE_OTHER,
+            detail=f'Já existe uma categoria cadastrada com o nome: {categoria_in.nome}'
+        )
 
     return categoria_out
-    
-    
+
+
 @router.get(
-    '/', 
+    '/',
     summary='Consultar todas as Categorias',
     status_code=status.HTTP_200_OK,
     response_model=list[CategoriaOut],
 )
-async def query(db_session: DatabaseDependency) -> list[CategoriaOut]:
-    categorias: list[CategoriaOut] = (await db_session.execute(select(CategoriaModel))).scalars().all()
-    
+async def query(
+    db_session: DatabaseDependency
+) -> list[CategoriaOut]:
+
+    categorias: list[CategoriaOut] = (
+        (
+            await db_session.execute(
+                select(CategoriaModel)
+            )
+        )
+        .scalars()
+        .all()
+    )
+
     return categorias
 
 
 @router.get(
-    '/{id}', 
+    '/{id}',
     summary='Consulta uma Categoria pelo id',
     status_code=status.HTTP_200_OK,
     response_model=CategoriaOut,
 )
-async def get(id: UUID4, db_session: DatabaseDependency) -> CategoriaOut:
+async def get(
+    id: UUID4,
+    db_session: DatabaseDependency
+) -> CategoriaOut:
+
     categoria: CategoriaOut = (
-        await db_session.execute(select(CategoriaModel).filter_by(id=id))
-    ).scalars().first()
+        (
+            await db_session.execute(
+                select(CategoriaModel).filter_by(id=id)
+            )
+        )
+        .scalars()
+        .first()
+    )
 
     if not categoria:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, 
+            status_code=status.HTTP_404_NOT_FOUND,
             detail=f'Categoria não encontrada no id: {id}'
         )
-    
+
     return categoria
